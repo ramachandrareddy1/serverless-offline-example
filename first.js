5f186a1710de78bcc7ec44ed6b852bf967768e29
Reddam Ramachandra Reddy <Reddam.Reddy@gds.ey.com>
2020-07-04 11:11:24 +0530

    testing chnages

--- first.js
+++ first.js
@@ -6,6 +6,7 @@ module.exports.hello = async event => {
     statusCode: 200,
     body: JSON.stringify(
       {
+        "data":"ram",
         message: 'Go Serverless v1.0! Your function executed successfully!',
         input: event,
       },
